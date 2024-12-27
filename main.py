04b1c0f9f8b43664ec09331198376a62f0c01543
jbrownsey <87074712+jbrownsey@users.noreply.github.com>
2024-12-27 16:16:53 +0000

    adding colab fix?

--- main.py
+++ main.py
@@ -7,13 +7,32 @@ from diffusers import StableDiffusionPipeline
 import base64
 from io import BytesIO
 
+import getpass
+import os
+import threading
+
+from pyngrok import ngrok, conf
+
 # Load model
 pipe = StableDiffusionPipeline.from_pretrained("stable-diffusion-v1-5/stable-diffusion-v1-5", torch_dtype=torch.float16)
 pipe.to("cuda")
 
+print("Enter your authtoken, which can be copied from https://dashboard.ngrok.com/get-started/your-authtoken")
+conf.get_default().auth_token = getpass.getpass()
+
 # Start flask app and set to ngrok
 app = Flask(__name__)
-run_with_ngrok(app)
+# run_with_ngrok(app)
+port = "5000"
+
+# Open a ngrok tunnel to the HTTP server
+public_url = ngrok.connect(port).public_url
+print(f" * ngrok tunnel \"{public_url}\" -> \"http://127.0.0.1:{port}\"")
+
+# Update any base URLs to use the public ngrok URL
+app.config["BASE_URL"] = public_url
+
+# ... Update inbound traffic via APIs to use the public-facing ngrok URL
 
 @app.route('/')
 def initial():
@@ -37,5 +56,8 @@ def generate_image():
   return render_template('index.html', generated_image=img_str)
 
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
+
+# Start the Flask server in a new thread
+threading.Thread(target=app.run, kwargs={"use_reloader": False}).start()
