36c809221e8b10fc8555a6f392b4bff2cf11d834
jbrownsey <87074712+jbrownsey@users.noreply.github.com>
2025-01-06 12:39:23 +0000

    is file being uploaded?

--- main.py
+++ main.py
@@ -483,8 +483,9 @@ def upload():
         filename = secure_filename(file.filename)
         filepath = os.path.join(app.config['UPLOAD_FOLDER'],filename)
         file.save(filepath)
-        html_data = evaluator.get_metric(filepath,company_name,reporting_period)
-        return render_template("file2.html", html_data=html_data)
+        return f"File {filepath} uploaded successfully",200
+        # html_data = evaluator.get_metric(filepath,company_name,reporting_period)
+        # return render_template("file2.html", html_data=html_data)
     
     return "Invalid file type", 400
 
